82e5a8c7fb721c768ba81d8c9141659b06503626
Hawkeye <40380233+H874589148@users.noreply.github.com>
2019-02-04 14:35:43 +0800

    群聊回复

--- newReply.py
+++ newReply.py
@@ -22,11 +22,21 @@ def get_response(msg):
 
 @itchat.msg_register(itchat.content.TEXT)
 def tuling_reply(msg):
-    print("message:%s" % msg['Text'])
+    print(msg.User['NickName'] +":"+ msg['Text'])
     defaultReply = 'I received: ' + msg['Text']
     #robots=['——By机器人图灵','——By机器人Hawkeye','——By反正不是本人']
     reply = get_response(msg['Text'])#+random.choice(robots)
+    #print (reply+"\n")
     return reply or defaultReply
+
+@itchat.msg_register([itchat.content.TEXT], isGroupChat=True)    #群消息的处理
+def print_content(msg):
+    if msg.User["NickName"]=='三人行'or msg.User["NickName"]=='另外一个你希望自动回复群的名字':    #这里可以在后面加更多的or msg.User["NickName"]=='你希望自动回复群的名字'
+        print(msg.User['NickName'] +":"+ msg['Text'])     #打印哪个群给你发了什么消息
+        print(get_response(msg['Text'])+"\n")           #打印机器人回复的消息
+        return get_response(msg['Text'])
+    else:                                         #其他群聊直接忽略
+        pass
 
 #itchat.auto_login(enableCmdQR=True)
 itchat.auto_login(hotReload=True)
